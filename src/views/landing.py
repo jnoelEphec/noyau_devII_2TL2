314f643af141002e780d17774bd539d362e38106
jonathannoel <jonathan.noel1@gmail.com>
2021-09-10 12:47:28 +0200

    Refactor some code and add a sorting package for dicts.

--- src/views/landing.py
+++ src/views/landing.py
@@ -10,15 +10,12 @@
         - La zone de contenu de nos channels après sélection d'un channel.
 """
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.screenmanager import Screen, ScreenManagerException
-from kivy.uix.scrollview import ScrollView
+from kivy.uix.screenmanager import Screen
 
 from src.config import config
-from src.models.channel import Channel
 from src.models.screens_manager import ScreensManager
+from src.views.channels_container import ChannelsContainer
+from src.views.widgets import RoomsContainer, ConversationContainer, InputsContainer
 
 Builder.load_file("{0}/header.kv".format(config.VIEWS_DIR))
 Builder.load_file("{0}/participants.kv".format(config.VIEWS_DIR))
@@ -26,107 +23,6 @@ Builder.load_file("{0}/conversation.kv".format(config.VIEWS_DIR))
 Builder.load_file("{0}/rooms.kv".format(config.VIEWS_DIR))
 Builder.load_file("{0}/channels.kv".format(config.VIEWS_DIR))
 Builder.load_file("{0}/landing.kv".format(config.VIEWS_DIR))
-
-
-class ChannelsListButton(Button):
-    pass
-
-
-class EmptyChannels(Label):
-    pass
-
-
-class ConversationContainer(ScrollView):
-
-    def __init__(self, room_id):
-        super(ConversationContainer, self).__init__()
-        self.room_id = room_id
-
-
-class RoomsContainer(ScrollView):
-    def __init__(self, channel_id):
-        super(RoomsContainer, self).__init__()
-        self.channel_id = channel_id
-
-
-class InputsContainer(BoxLayout):
-    pass
-
-
-class ChannelsContainer(ScrollView):
-
-    def __init__(self):
-        super(ChannelsContainer, self).__init__()
-        self.content = self.ids.channels_content
-        self.sm = ScreensManager()
-
-        self.init_channels_list()
-
-    def init_channels_list(self):
-        """
-            [BASE]
-            Initialise la liste des channels disponibles.
-            Si l'utilisateur fait partie de channels, les affiche dans le container concerné.
-            Sinon, un message s'affiche précisant que l'utilisateur ne fait partie d'aucun channel.
-        """
-        self.content.clear_widgets()
-
-        channels_list = self.get_channels_list()
-        landing_screen = None
-
-        try:
-            landing_screen = self.sm.get_screen("landing")
-        except ScreenManagerException:
-            pass
-
-        if channels_list:
-            for channel in channels_list:
-                channel_label = ChannelsListButton(text=channel.name)
-                channel_label.bind(on_press=lambda a: landing_screen.display_rooms(channel.identifier))
-                self.content.add_widget(channel_label)
-        else:
-            self.content.add_widget(EmptyChannels())
-
-    def get_channels_list(self):
-        """
-            [BASE]
-            Récupère la liste des channels depuis la banque de données.
-            :return: list : La liste des channels (objets) auxquels nous appartenons.
-        """
-
-        list_of_channels = []
-
-        data_from_db = {
-            "ABCDEF": {
-                "name": "Pis",
-                "icon_path": "",
-                "participants": {}
-            },
-            "GHIJKL": {
-                "name": "Pan",
-                "icon_path": "",
-                "participants": {}
-            },
-            "THIRD_UNIQUE_IDENTIFIER": {
-                "name": "Douille",
-                "icon_path": "",
-                "participants": {}
-            }
-        }
-
-        if data_from_db:
-
-            for channel_id in data_from_db:
-                channel_name = data_from_db[channel_id]["name"]
-                icon_path = data_from_db[channel_id]["icon_path"]
-                participants = data_from_db[channel_id]["participants"]
-
-                inst = Channel(channel_id, channel_name, icon_path=icon_path, participants=participants)
-                list_of_channels.append(inst)
-
-            return list_of_channels
-
-        return None
 
 
 class LandingScreen(Screen):
@@ -140,7 +36,7 @@ class LandingScreen(Screen):
 
     def redirect_to_href(self, href):
         """
-            A modifier vers le header !
+            ! A modifier vers le header !
         """
         self.sm.redirect(href)
 
